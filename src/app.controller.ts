6e51623a4d19d2fd1c985dba9e3c35619f260bda
Maxim <zeetreex@gmail.com>
2024-01-14 12:21:52 +0200

    added functionality to upload one file per request and saving it to uploads folder, added download functionality for uploaded files

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from "@nestjs/common";
+import { AppService } from "./app.service";
+import { FileInterceptor } from "@nestjs/platform-express";
 
-@Controller()
+@Controller("api/files")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post("upload")
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return await this.appService.uploadFile(file);
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getFiles() {
+    return await this.appService.getFiles();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from "@nestjs/common";
+import * as path from "path";
+import * as fs from "fs";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async uploadFile(file: Express.Multer.File) {
+    const uploadDirectory = "uploads";
+
+    if (!fs.existsSync(uploadDirectory)) {
+      fs.mkdirSync(uploadDirectory);
+    }
+
+    const fileName = file.originalname;
+    const filePath = path.join(uploadDirectory, fileName);
+
+    if (fs.existsSync(path.join(uploadDirectory, fileName))) {
+      throw new ConflictException("File already exists");
+    }
+
+    fs.writeFileSync(filePath, file.buffer);
+  }
+
+  async getFiles() {
+    const directoryPath = "uploads";
+
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath);
+    }
+
+    return fs.readdirSync(directoryPath);
   }
 }
